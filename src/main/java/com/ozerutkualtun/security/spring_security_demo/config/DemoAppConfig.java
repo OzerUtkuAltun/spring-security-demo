7583aa1d52c30b0c035be0982f03fdb27828e8cb
Özer Altun <ozerutkualtun@gmail.com>
2022-07-21 10:52:21 +0300

    spring security with JDBC (Önemli)

--- src/main/java/com/ozerutkualtun/security/spring_security_demo/config/DemoAppConfig.java
+++ src/main/java/com/ozerutkualtun/security/spring_security_demo/config/DemoAppConfig.java
@@ -1,16 +1,31 @@
 package com.ozerutkualtun.security.spring_security_demo.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Objects;
+import java.util.logging.Logger;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.ozerutkualtun.security.spring_security_demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
+
+    // set up variable to hold properties
+
+    @Autowired
+    private Environment env;
 
     @Bean
     public ViewResolver viewResolver() {
@@ -21,5 +36,36 @@ public class DemoAppConfig {
         internalResourceViewResolver.setSuffix(".jsp");
 
         return internalResourceViewResolver;
+    }
+
+    // defining a bean for security data source
+
+    @Bean
+    public DataSource securityDataSource() {
+
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+        try {
+            securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+
+        securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        securityDataSource.setUser(env.getProperty("jdbc.user"));
+        securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+        securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+        return securityDataSource;
+    }
+
+    private int getIntProperty(String propertyName) {
+
+        return Integer.parseInt(Objects.requireNonNull(env.getProperty(propertyName)));
+
     }
 }
--- src/main/java/com/ozerutkualtun/security/spring_security_demo/config/SecurityConfig.java
+++ src/main/java/com/ozerutkualtun/security/spring_security_demo/config/SecurityConfig.java
@@ -1,27 +1,28 @@
 package com.ozerutkualtun.security.spring_security_demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    // adding a reference to our security datasource
+
+    @Autowired
+    private DataSource securityDataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        // not: User.withDefaultPasswordEncoder is deprecated.
+        auth.jdbcAuthentication().dataSource(securityDataSource);
 
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-
-        auth.inMemoryAuthentication()
-                .withUser(users.username("utku").password("test123").roles("ADMIN", "EMPLOYEE","MANAGER"))
-                .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-                .withUser(users.username("susan").password("test123").roles("EMPLOYEE", "MANAGER"));
     }
 
     // bu metod login formu customize etmek için kullanılacak
